58c16a74ea99248a8b6b2cd8515529123d80a0bd
TheKinng96 <fengyuan.yap@example.com>
2020-09-27 21:09:48 +0900

    implemented badge for span

--- src/components/ui/BaseBadge.vue
+++ src/components/ui/BaseBadge.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <span class="badge" :class="type">{{ text }}</span>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ['type', 'title'],
+  computed: {
+    text() {
+      return this.title.toUpperCase();
+    }
+  }
+};
 </script>
 
 <style scoped>
