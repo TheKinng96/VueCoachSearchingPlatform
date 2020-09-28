2196d8868880c2b500d39489db6f9f34abb03fa9
TheKinng96 <fengyuan.yap@example.com>
2020-09-28 21:38:44 +0900

    vuex to add form data to push a new coach

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,23 @@
 <template>
-  Register
+  <section>
+    <base-card>
+      <h2>Register as a coach</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm';
+
+export default {
+  components: {
+    CoachForm
+  },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data);
+    }
+  }
+};
+</script>
