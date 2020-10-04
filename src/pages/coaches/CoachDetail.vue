167ea2ce02ba2596a1c2ae50f22f908db75ea2fc
TheKinng96 <fengyuan.yap@example.com>
2020-10-04 21:43:20 +0900

    added transition to all pages

--- src/pages/coaches/CoachDetail.vue
+++ src/pages/coaches/CoachDetail.vue
@@ -1,30 +1,32 @@
 <template>
-  <section>
-    <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Interested? Reach out now!</h2>
-        <base-button link :to="ContactLink">Contact</base-button>
-      </header>
-      <router-view></router-view>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge
-        v-for="area in areas"
-        :key="area"
-        :type="area"
-        :title="area"
-      ></base-badge>
-      <p>{{ description }}</p>
-    </base-card>
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <base-button link :to="ContactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        ></base-badge>
+        <p>{{ description }}</p>
+      </base-card>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -32,12 +34,12 @@ export default {
   props: ['id'],
   data() {
     return {
-      selectedCoach: null
+      selectedCoach: null,
     };
   },
   created() {
     this.selectedCoach = this.$store.getters['coaches/coaches'].find(
-      coach => coach.id === this.id
+      (coach) => coach.id === this.id
     );
   },
   computed: {
@@ -55,7 +57,7 @@ export default {
     },
     description() {
       return this.selectedCoach.description;
-    }
-  }
+    },
+  },
 };
 </script>
